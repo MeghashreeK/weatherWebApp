3c7950adabb2dbe378d6a427e336e9173c4f15bb
MeghashreeK <kundermeghashree279@gmail.com>
2024-07-04 00:15:00 +0530

    removed console

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 async function checkWeather(city){
     const response=await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data= await response.json();
-    console.log(data)
+    // console.log(data)
     document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+"°c";
     document.querySelector(".city").innerHTML=data.name;
     document.querySelector(".humidity").innerHTML= data.main.humidity+" %";
     document.querySelector(".Wind").innerHTML=data.wind.speed+" km/hr";
-    console.log(data.weather[0].main);
+    // console.log(data.weather[0].main);
     if(data.weather[0].main=="Clear"){
         weatherIcon.src="clear.png";
     }else if(data.weather[0].main=="Clouds"){
